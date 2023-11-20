a27fde783c7469f64c33370ce3d9338516b261c2
groovy-sky <groovy-sky>
2023-11-20 14:51:25 +0200

    main part done and restruct

--- cmd/knock2spot/main.go
+++ cmd/knock2spot/main.go
@@ -33,8 +33,40 @@ func parseResourceId(resourceId string) (subscriptionId, resourceGroup, resource
 	return subscriptionId, resourceGroup, resourceProvider, resourceName
 }
 
+// Returns form to input NSG Id, destination port, rule number and rule name
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "default.html")
+	w.Write([]byte(`<!DOCTYPE html>
+	<html>
+	<head>
+	  <meta charset="UTF-8" />
+	</head>
+	<body>
+	<div>
+	<form action="whitelistip" method="POST">
+		<label for="nsgid">NSG's ID:</label><input type="text" id="nsgid" name="nsgid" value="" required /> <br><br>
+		
+		<label for="dstport">Destination's Port:</label><input type="text" id="dstport" value="" name="dstport" required /><br><br>
+	
+		<label for="rulenumber">Security Rule Number:</label><input type="text" id="rulenumber" name="rulenumber" value="" required><br><br>
+	
+		<label for="rulename">Security Rule Name:</label><input type="text" id="rulename" name="rulename" value="" required><br><br>
+		
+		<input type="submit" value="Submit">
+	</form>
+	</div>
+	</body>
+	</html>`))
+}
+
+// Returns client's IP address
+func myIpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("X-Forwarded-For") != "" {
+		w.Write([]byte(strings.Split(r.Header.Get("X-Forwarded-For"), ":")[0]))
+	} else if r.Header.Get("Host") != "" {
+		w.Write([]byte(strings.Split(r.Header.Get("Host"), ":")[0]))
+	} else if r.RemoteAddr != "" {
+		w.Write([]byte(strings.Split(r.RemoteAddr, ":")[0]))
+	}
 }
 
 // Takes incoming IP address and adds it to NSG
@@ -152,9 +184,9 @@ func allowInbound(ctx context.Context, securityRulesClient *armnetwork.SecurityR
 }
 
 func main() {
-	httpInvokerPort, exists := os.LookupEnv("FUNCTIONS_HTTPWORKER_PORT")
+	httpInvokerPort, exists := os.LookupEnv("HTTP_PORT")
 	if exists {
-		fmt.Println("FUNCTIONS_HTTPWORKER_PORT: " + httpInvokerPort)
+		fmt.Println("HTTP_PORT: " + httpInvokerPort)
 	} else {
 		httpInvokerPort = "8080"
 	}
@@ -162,6 +194,7 @@ func main() {
 	// Make default hander which returns default.html
 	mux.HandleFunc("/", defaultHandler)
 	mux.HandleFunc("/whitelistip", whitelistipHandler)
-	log.Println("Go server Listening...on httpInvokerPort:", httpInvokerPort)
+	mux.HandleFunc("/myip", myIpHandler)
+	log.Println("[INF] Listening on port ", httpInvokerPort)
 	log.Fatal(http.ListenAndServe(":"+httpInvokerPort, mux))
 }

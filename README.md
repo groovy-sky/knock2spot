89d220089ee30f9ad708f4a6e56566cd72867fa1
groovy-sky <groovy-sky>
2023-11-29 23:56:58 +0200

    added paas support

--- README.md
+++ README.md
@@ -1,23 +1,30 @@
-# Azure Functions custom handler in Go
+# knock2spot
 
 ![](/logo.svg)
 
-This repository contains an example of an Azure Functions custom handler in Go. **Detailed instruction how-to run you can find [here](https://github.com/groovy-sky/azure/tree/master/func-custom-handler-00#introduction).**
+## What is this?
 
-You can use Azure Cloud Shell in Bash mode for deploying and building Functions environment. Just execute script below:
+This project is a small website (written on Go), which can be used to seamesly whitelist your IP address on Azure resources. Currenty you can use it to:
+1. Reveal your IP address
+2. Whitelist your IP address on Azure Paas firewall (currently supports only Storage Account)
+3. Whitelist your IP address on Network Security Group for specified destination port
 
-```
-deploy_region="westeurope"                                                                                    
-deploy_group="go-custom-function"                                                                            
-az group create -l $deploy_region -n $deploy_group                                                            
-function=$(az deployment group create --resource-group $deploy_group --template-uri https://raw.githubusercontent.com/groovy-sky/azure-func-go-handler/master/Template/azuredeploy.json | jq -r '. | .properties | .dependencies | .[] | .resourceName')                                                     
-[ ! -d "azure-func-go-handler/.git" ] && git clone https://github.com/groovy-sky/azure-func-go-handler        
-cd azure-func-go-handler/Function && git pull                                                                 
-go build *.go && func azure functionapp publish $function --no-build --force
+## How to use it?
 
+You don't to compile this project, instead you can use [the Docker image](https://hub.docker.com/repository/docker/gr00vysky/knock2spot) or build it yourself using the [Dockerfile](/Dockerfile).
 
-```
+Easiest way how you can use this project is to deploy it to Azure App Service or Azure Container App. After deployment you'll need to assign managed identity to the app and grant permissions to the managed identity to the resources you want to whitelist your IP address on.
 
-If the publishing was successful you can validate a result by accessing a newly created function.
+After App is deployed you can use it by navigating to the root of the website. You'll see 3 forms:
+1. For NSG whitelisting. Requires NSG resource ID, destination port, rule priority and rule name.
+2. For PaaS whitelisting. Requires resource's ID.
+3. For IP address revealing.
 
+## How it works?
 
+This project uses [Azure SDK for Go](https://github.com/Azure/azure-sdk-for-go) to interact with Azure in a following way:
+1. It tries in different way to get Azure token - from MSI endpoint, environment variables or logged Azure CLI session.
+
+2. It parses requester IP address from request headers and tries to whitelist it on specified resources.
+
+3. Using token and requester's IP address it tries to append IP address to specified NSG rule or PaaS firewall.
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage"
 )
 
 // Checks that input matches Azure Resource Id format
@@ -27,6 +28,7 @@ func validateResId(input string) bool {
 
 // Gets Azure Resource's names of subscription, group, resource type etc.
 func parseResourceId(resourceId string) (subscriptionId, resourceGroup, resourceProvider, resourceName string) {
+	resourceId = strings.ToLower(resourceId)
 	// takes Azure resource Id and parses sub id, group, resource type and name
 	parts := strings.Split(resourceId, "/")
 	subscriptionId = parts[2]
@@ -36,21 +38,76 @@ func parseResourceId(resourceId string) (subscriptionId, resourceGroup, resource
 	return subscriptionId, resourceGroup, resourceProvider, resourceName
 }
 
-// Returns form to input NSG Id, destination port, rule number and rule name
+// Draws default page which contains links to all related pages
+// Also contains a form to submit data to whitelistipHandler
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`
-	<div>
-	<form action="whitelistip" method="POST">
-		<label for="resid">NSG's ID:</label><input type="text" id="resid" name="resid" value="" required /> <br><br>
-		
-		<label for="dstport">Destination's Port:</label><input type="text" id="dstport" value="" name="dstport" required /><br><br>
-	
-		<label for="rulenumber">Security Rule Number:</label><input type="text" id="rulenumber" name="rulenumber" value="" required><br><br>
-	
-		<label for="rulename">Security Rule Name:</label><input type="text" id="rulename" name="rulename" value="" required><br><br>
-		
-		<input type="submit" value="Submit">
-	</form>`))
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>knock2spot</title>
+		<link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/groovy-sky/knock2spot/master/logo.svg">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+		<style>
+			/* Your custom CSS styles here */
+			form {
+				margin: 20px;
+				padding: 10px;
+				display: inline-block;
+			}
+			big {
+				margin: 5px;
+				align: center;
+				style: bold;
+			}
+			fieldset {
+				border: none;
+				box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+				margin-bottom: 10px;
+				padding: 5px;
+				width: 20%; 
+			}
+			fieldset:nth-child(odd) {
+				background-color: #f5f5f5; /* Lighter color for odd fieldsets */
+			}
+			fieldset:nth-child(even) {
+				background-color: #ebebeb; /* Darker color for even fieldsets */
+			}
+		</style>
+	</head>
+	<body>
+		<fieldset>	
+			<form action="whitelistip" method="POST">
+			<legend>Whitelist IP for NSG</legend>
+				<label for="resid">NSG's ID:</label>
+				<input type="text" id="resid" name="resid" value="" required /><br>
+				<label for="dstport">Destination's Port:</label>
+				<input type="text" id="dstport" value="" name="dstport" required /><br>
+				<label for="rulenumber">Security Rule Number:</label>
+				<input type="text" id="rulenumber" name="rulenumber" value="" required><br>
+				<label for="rulename">Security Rule Name:</label>
+				<input type="text" id="rulename" name="rulename" value="" required><br>
+				<input type="submit" value="Submit">
+			</form>
+		</fieldset>
+		<fieldset>
+			<form action="whitelistip" method="POST">
+			<legend>Whitelist IP for PaaS</legend>
+				<label for="resid">PaaS resource's ID:</label>
+				<input type="text" id="resid" name="resid" value="" required /><br>
+				<input type="submit" value="Submit">
+			</form>
+		</fieldset>
+		<fieldset>
+			<form action="myip" method="GET">
+			<legend>Show my IP</legend>
+				<input type="submit" value="Open"><br>
+			</form>
+		</fieldset>
+	</body>
+	</html>
+	`))
 }
 
 // Parse IP from X-Forwarded-For, Host or RemoteAddr headers
@@ -73,20 +130,19 @@ func myIpHandler(w http.ResponseWriter, r *http.Request) {
 // Takes incoming IP address and adds it to NSG
 func whitelistipHandler(w http.ResponseWriter, r *http.Request, cred *azidentity.ChainedTokenCredential) {
 	var ip, resid, dstPort, ruleName, ruleNumberStr string
-
+	var err error
 	// Get the request body and parse input data generated from default.html in case of POST request
 	switch r.Method {
 	case "POST":
 		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
+			log.Fatal("[ERR] Invalid Parsing of Form")
 		}
 		resid = r.FormValue("resid")
 		dstPort = r.FormValue("dstport")
 		ruleName = r.FormValue("rulename")
 		ruleNumberStr = r.FormValue("rulenumber")
 	default:
-		resid = os.Getenv("NSG_ID")
+		resid = os.Getenv("RES_ID")
 		dstPort = os.Getenv("DST_PORT")
 		ruleName = os.Getenv("RULE_NAME")
 		ruleNumberStr = os.Getenv("RULE_NUMBER")
@@ -103,10 +159,13 @@ func whitelistipHandler(w http.ResponseWriter, r *http.Request, cred *azidentity
 	// Get client's IP address
 	ip = parseIp(r)
 
+	// Create context
 	ctx := context.Background()
 
+	fmt.Println(resourceType)
+
 	switch resourceType {
-	case "Microsoft.Network/networkSecurityGroups":
+	case "microsoft.network/networksecuritygroups":
 		var networkClientFactory *armnetwork.ClientFactory
 		ruleNumber, err := strconv.Atoi(ruleNumberStr)
 		if err != nil {
@@ -119,11 +178,81 @@ func whitelistipHandler(w http.ResponseWriter, r *http.Request, cred *azidentity
 		client := networkClientFactory.NewSecurityRulesClient()
 
 		_, err = addNsgRule(ctx, client, resourceGroup, resourceName, ruleName, "*", dstPort, "Tcp", ip, "*", int32(ruleNumber))
+
+	case "microsoft.storage/storageaccounts":
+		storageAccountsClient, err := armstorage.NewAccountsClient(subscriptionId, cred, nil)
+
 		if err != nil {
 			log.Fatal(err)
 		}
+		_, err = addStorageRule(ctx, storageAccountsClient, resourceGroup, resourceName, ip)
+	default:
+		log.Fatal("[ERR] Invalid Resource Type")
+	}
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("[INF] IP", ip, "whitelisted for", resid)
 		w.Write([]byte(""))
 	}
+
+}
+
+// Creates a new rule in Storage Account Firewall
+func addStorageRule(ctx context.Context, storageAccountsClient *armstorage.AccountsClient, rgName, storageAccountName, ip string) (bool, error) {
+	var newIpRuleSet []*armstorage.IPRule
+	var newIps []*string
+
+	log.Println("[INF] Trying to modify", storageAccountName, "storage account")
+	var ok bool
+	// Check if rule already exists. If it does, appends the new source IP to the existing rule
+	storageAccount, err := storageAccountsClient.GetProperties(ctx, rgName, storageAccountName, &armstorage.AccountsClientGetPropertiesOptions{Expand: nil})
+
+	if err != nil {
+		log.Fatal("[ERR] Cannot get storage account properties: ", err)
+	}
+
+	if storageAccount.Properties != nil && storageAccount.Properties.NetworkRuleSet != nil && storageAccount.Properties.NetworkRuleSet.IPRules != nil {
+		for _, ipRule := range storageAccount.Properties.NetworkRuleSet.IPRules {
+			newIps = append(newIps, ipRule.IPAddressOrRange)
+		}
+	}
+
+	// Check if IP is already whitelisted
+	for _, oldIp := range newIps {
+		if *oldIp == ip {
+			log.Println("[INF] IP", ip, "is already whitelisted")
+			return ok, nil
+		}
+	}
+	newIps = append(newIps, &ip)
+
+	for _, ip := range newIps {
+		newRule := &armstorage.IPRule{
+			Action:           &[]string{"Allow"}[0],
+			IPAddressOrRange: ip,
+		}
+		newIpRuleSet = append(newIpRuleSet, []*armstorage.IPRule{newRule}...)
+
+	}
+
+	storageAccount.Properties.NetworkRuleSet.IPRules = newIpRuleSet
+
+	// Disable full access and limit to certain IPs
+	storageAccount.Properties.NetworkRuleSet.DefaultAction = &[]armstorage.DefaultAction{armstorage.DefaultActionDeny}[0]
+	storageAccount.Properties.PublicNetworkAccess = &[]armstorage.PublicNetworkAccess{armstorage.PublicNetworkAccessEnabled}[0]
+
+	// Create or update the security rule
+	_, err = storageAccountsClient.Update(ctx,
+		rgName,
+		storageAccountName,
+		armstorage.AccountUpdateParameters{Properties: &armstorage.AccountPropertiesUpdateParameters{NetworkRuleSet: storageAccount.Properties.NetworkRuleSet, PublicNetworkAccess: storageAccount.Properties.PublicNetworkAccess}},
+		nil)
+
+	if err != nil {
+		return ok, fmt.Errorf("[ERR] Couldn't whitelist IP: %v", err)
+	}
+	return true, nil
 }
 
 // Creates a new allow inbound security rule in NSG
@@ -193,7 +322,6 @@ func addNsgRule(ctx context.Context, securityRulesClient *armnetwork.SecurityRul
 	if err != nil {
 		return ok, fmt.Errorf("cannot get security rule create or update future response: %v", err)
 	}
-	log.Println("[INF] Successfully updated rule", ruleName)
 	return true, nil
 }
 
@@ -228,7 +356,6 @@ func main() {
 		httpInvokerPort = "8080"
 	}
 	mux := http.NewServeMux()
-	// Make default hander which returns default.html
 	mux.HandleFunc("/", defaultHandler)
 	mux.HandleFunc("/whitelistip", func(w http.ResponseWriter, r *http.Request) {
 		whitelistipHandler(w, r, login)

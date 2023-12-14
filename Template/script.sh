f7f3a1bbfbf79922eb97dce7e10f7f41c16a5b63
groovy-sky <groovy-sky>
2023-12-14 20:26:04 +0200

    working on deployment

--- Template/script.sh
+++ Template/script.sh
@@ -1,7 +1,19 @@
-appResGroup="delete-me-rg"
+#!/bin/bash
 
-appResId=""
+appResGroup="knock-group"
+appRegion="westeurope"
+
+echo "## Resource Group deployment"
+az group create -l $appRegion -n $appResGroup
+
+echo "## Initial App deployment"
+deployResult=$(az deployment group create --resource-group $appResGroup --template-uri https://raw.githubusercontent.com/groovy-sky/knock2spot/master/Template/azuredeploy.json)
+
+echo "## Assigning Contributor role to the App"
+
+appResId=$(echo $deployResult | jq -r '.properties.outputs.containerAppId.value')
+nsgId=$(echo $deployResult | jq -r '.properties.outputs.nsgId.value')
 
 identityId=$(az resource show --id $appResId --query "identity.principalId" -o tsv)
 
-az role assignment create --assignee $identityId --role 'Contributor' --scope $appResId
+az role assignment create --assignee $identityId --role 'Contributor' --scope $nsgId
